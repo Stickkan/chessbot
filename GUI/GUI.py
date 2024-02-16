efd2d83aa62f0f373f07ccb42e050c8397086b3f
Svante jbBjorkman <sbn20007@student.mdu.se>
2024-02-16 12:18:19 +0100

    fucked it up bre

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -2,11 +2,12 @@ import pygame
 import chess
 import math
 
-pygame.init()
+
 #init av skärkm
 X = 800 
 Y = 800
 scrn = pygame.display.set_mode((X,Y))
+pygame.init()
 
 
 #basic färger från brädan
@@ -22,17 +23,17 @@ b = chess.Board()
 #pjäser
 
 pieces = {'p': pygame.image.load('b_pawn.png').convert(),
-          'n': pygame.image.load('b_knight.png').convert,
-          'b': pygame.image.load('b_bishop.png').convert,
-          'r': pygame.image.load('b_rook.png').convert,
-          'q': pygame.image.load('b_queen.png').convert,
-          'k': pygame.image.load('b_king.png').convert,
-          'P': pygame.image.load('w_pawn.png').convert,
-          'N': pygame.image.load('w_knight.png').convert,
-          'B': pygame.image.load('w_bishop.png').convert,
-          'R': pygame.image.load('w_rook.png').convert,
-          'Q': pygame.image.load('w_queen.png').convert,
-          'K': pygame.image.load('w_king.png').convert,
+          'n': pygame.image.load('b_knight.png').convert(),
+          'b': pygame.image.load('b_bishop.png').convert(),
+          'r': pygame.image.load('b_rook.png').convert(),
+          'q': pygame.image.load('b_queen.png').convert(),
+          'k': pygame.image.load('b_king.png').convert(),
+          'P': pygame.image.load('w_pawn.png').convert(),
+          'N': pygame.image.load('w_knight.png').convert(),
+          'B': pygame.image.load('w_bishop.png').convert(),
+          'R': pygame.image.load('w_rook.png').convert(),
+          'Q': pygame.image.load('w_queen.png').convert(),
+          'K': pygame.image.load('w_king.png').convert(),
           }
 
 def update(scrn,board):
@@ -57,7 +58,7 @@ def main(BOARD):
     pygame.display.set_caption('Chess')
 
     index_moves = []
-    ##moves = []
+    moves = []
 
     status = True
     while (status):
@@ -105,6 +106,6 @@ def main(BOARD):
             print(BOARD.outcome())
             status = False
             print(BOARD)
-    pygame.display.update()    
+    ##pygame.display.update()    
     pygame.quit()  
 main(b)
